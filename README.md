9d95a8b4df98ae28e2b9210bec580a96e486ef94
Jake Gaylor <jgaylor@fsw.com>
2017-04-12 01:07:21 -0600

    bang on it

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ To run locally, you need to indicate to the go binary that we are external to a 
 
 `go run main.go --external --kubeconfig /Users/jake/.kube/config`
 
+Test the WS api
+
+`curl -i -N -H "Connection: Upgrade" -H "Upgrade: websocket" -H "Sec-Websocket-Version: 13" -H "Sec-Websocket-Key: jake" http://localhost:8080/ws/pods/load-demo`
+
+
 # How to ship a new release?
 
 Publish code to github on master and docker hub's automated build service will publish a docker image @ `jhgaylor/k8s-load-demo-api`. The deployment pulls it from docker's public repository.
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  // "encoding/json"
+  "encoding/json"
   "flag"
   "fmt"
+  "github.com/gorilla/websocket"
   "gopkg.in/gin-gonic/gin.v1"
-  // "html"
   metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
   "k8s.io/client-go/kubernetes"
   _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -14,6 +14,34 @@ import (
   "net/http"
   "os"
 )
+
+var upgrader = websocket.Upgrader{}
+
+func bindWsHandlers(k8s *kubernetes.Clientset, router *gin.Engine) {
+  router.GET("/ws/pods/:namespace", func(c *gin.Context) {
+    namespace := c.Param("namespace")
+    conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+    if err != nil {
+      fmt.Println("Failed to set websocket upgrade: %+v", err)
+      return
+    }
+    watch, err := k8s.CoreV1().Pods(namespace).Watch(metav1.ListOptions{})
+    if err != nil {
+      fmt.Println("Failed to get a watch: %+v", err)
+      return
+    }
+    ch := watch.ResultChan()
+    for {
+      msg := <-ch
+      msgJson, err := json.Marshal(msg)
+      if err != nil {
+        fmt.Println("Failed converting event to json: %+v", err)
+        return
+      }
+      conn.WriteMessage(websocket.TextMessage, msgJson)
+    }
+  })
+}
 
 func bindHttpHandlers(k8s *kubernetes.Clientset, router *gin.Engine) {
   router.GET("/ping", func(c *gin.Context) {
@@ -25,14 +53,7 @@ func bindHttpHandlers(k8s *kubernetes.Clientset, router *gin.Engine) {
     if err != nil {
       c.String(http.StatusInternalServerError, err.Error())
     }
-    // podsJson, err := json.Marshal(pods.Items)
-    // if err != nil {
-    // fmt.Fprintf(w, "JSON encoding error, %v", err.Error())
-    // }
     c.JSON(http.StatusOK, pods)
-    // fmt.Fprintf(w, "There are %d pods in the cluster\n", len(pods.Items))
-    // fmt.Fprintf(w, "\n\n%v\n\n", string(podsJson))
-    // fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
   })
 }
 
@@ -61,7 +82,7 @@ func getKubernetesClient(external bool, kubeconfig string) (*kubernetes.Clientse
 }
 
 func main() {
-  external := flag.Bool("external", false, "are we inside of a cluster")
+  external := flag.Bool("external", false, "Are we outside of a cluster?")
   kubeconfig := flag.String("kubeconfig", "/Users/jake/.kube/config", "absolute path to the kubeconfig file")
   flag.Parse()
 
@@ -78,6 +99,6 @@ func main() {
   router := gin.Default()
 
   bindHttpHandlers(k8sClient, router)
-
+  bindWsHandlers(k8sClient, router)
   router.Run(fmt.Sprintf(":%v", port))
 }
