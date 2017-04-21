48690b373288d9aa65897068ca2ac617299f9db3
Jake Gaylor <jgaylor@fsw.com>
2017-04-21 01:22:59 -0600

    capture restart count in ui

--- public/components.jsx
+++ public/components.jsx
@@ -17,6 +17,9 @@ class Pod extends React.Component {
     const pod_classes = `pod ${pod.metadata.labels.role}`
     const request_total = pod.metrics && pod.metrics['http_requests_total{code="200",handler="PasswordHasher",method="get"}'] || null
     const uptime = moment().diff(pod.metadata.creationTimestamp, 'seconds')
+    const restarts = pod.status.containerStatuses.reduce((acc, status) => {
+      return status.restartCount + acc
+    }, 0)
     var requests_per_second = '--'
     if (request_total || request_total === 0) {
       requests_per_second = (request_total / uptime).toFixed(1)
@@ -26,6 +29,7 @@ class Pod extends React.Component {
       <div className={pod_classes}>
         <p>{pod.metadata.labels.role}</p>
         <p>R/s {requests_per_second}</p>
+        <p>Restarts {restarts}</p>
       </div>
     )
   }
